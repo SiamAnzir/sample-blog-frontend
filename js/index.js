5630876b035f8849a11a9fb143b902f2eef24d04
Ragib  Ahsanul <raghibhuda@gmail.com>
2020-06-08 01:16:14 +0600

    [Refactored] functions and button id added

--- js/index.js
+++ js/index.js
@@ -1,47 +1,69 @@
 $(document).ready(function() {
-    function getUserPostsUi(posts) {
-        $(".pageloader").fadeOut("slow");
 
-        var str = "";
-        var i;
-        for (i = 2; i < 5; i++) {
-
-
-            str = str + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
-                '<div class="card" style="height:320px;width:250px">' +
-                '<h4 class="card-header"><b>' + posts[i].title.substr(0, 30) + '</b></h4>' +
-                '<div class="card-body">' +
-                '<h5 class="card-title"><b> Description</b></h5>' +
-                '<p class="card-text">' + posts[i].body.substr(0, 80) + '.........' + '</p>' +
-                '</div>' +
-                '<div class="card-footer">' +
-                '<button type="button" class="btn btn-primary" data-id="" data-toggle="modal" data-target="#exampleModal">Read More</button>' +
-                '</div>' +
-                '</div>' +
-                '</div>';
-        }
-        $("#cardId").html(str);
-    }
-    var $post;
-    //var i = 0;
-    $.ajax({
+	$.ajax({
         method: 'GET',
         url: 'https://jsonplaceholder.typicode.com/posts'
     }).done(function(post) {
-        console.log(post);
 
         getUserPostsUi(post);
     }).fail(function(error) {
         console.log(error);
     });
 
+
+     $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos'
+    }).done(function(todo) {
+        console.log(todo);
+
+        getUserTodosUi(todo);
+    }).fail(function(error) {
+        console.log(error);
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/photos'
+    }).done(function(photo) {
+        console.log(photo[0]);
+
+        getUserPhotosUi(photo);
+    }).fail(function(error) {
+        console.log(error);
+    });
+
+
+    function getUserPostsUi(posts) {
+        $(".pageloader").fadeOut("slow");
+
+        let singlePostCard = "";
+        for (let i = 1; i < 5; i++) {
+            singlePostCard = singlePostCard + 
+            '<div class="col-lg-4 col-md-6 col-sm-12" >' +
+                '<div class="card" style="height:320px;width:250px">' +
+                	'<h4 class="card-header"><b>' + posts[i].title.substr(0, 30) + '</b></h4>' +
+                '<div class="card-body">' +
+                	'<h5 class="card-title"><b> Description</b></h5>' +
+                	'<p class="card-text">' + posts[i].body.substr(0, 80) + '.........' + '</p>' +
+                '</div>' +
+                '<div class="card-footer">' +
+                	'<button type="button"+ data-id="'+i+'" class="btn btn-primary"  data-toggle="modal" data-target="#exampleModal">Read More</button>' +
+                '</div>' +
+                '</div>' +
+            '</div>';
+        }
+        $("#postContainer").html(singlePostCard);
+    }
+   
+    
     function getUserPhotosUi(photos) {
         $(".pageloader").fadeOut("slow");
-        var pht = "";
-        var i;
-        for (i = 0; i < 6; i++) {
-
-            pht = pht + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
+        let singlePhoto = "";
+        
+        for (let i = 0; i < 6; i++) {
+            singlePhoto = singlePhoto + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
                 '<div class="card-group">' +
                 '<div class="card border-dark mb-3" style="max-width: 18rem;">' +
                 '<img class="card-img-top" src="' + photos[i].thumbnailUrl + '" alt="Card image cap">' +
@@ -58,29 +80,18 @@ $(document).ready(function() {
 
         }
 
-
-        $("#photoId").html(pht);
+        $("#photoContainer").html(singlePhoto);
     }
-    var $photo;
-    //var i = 0;
-    $.ajax({
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/photos'
-    }).done(function(photo) {
-        console.log(photo[0]);
-
-        getUserPhotosUi(photo);
-    }).fail(function(error) {
-        console.log(error);
-    });
+   
+    
 
     function getUserTodosUi(todos) {
         $(".pageloader").fadeOut("slow");
-        var tds = "";
-        var i;
-        for (i = 0; i < 10; i++) {
+        let singleTodo = "";
+        
+        for (let i = 0; i < 10; i++) {
 
-            tds = tds + '<tr>' +
+            singleTodo = singleTodo + '<tr>' +
                 '<th scope="row">' + todos[i].id + '</th>' +
                 '<td>' + todos[i].title + '</td>' +
                 '<td>' +
@@ -93,18 +104,7 @@ $(document).ready(function() {
         }
 
 
-        $("#todoId").html(tds);
+        $("#todoContainer").html(singleTodo);
     }
-    var $post;
-    //var i = 0;
-    $.ajax({
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/todos'
-    }).done(function(todo) {
-        console.log(todo);
-
-        getUserTodosUi(todo);
-    }).fail(function(error) {
-        console.log(error);
-    });
+   
 });
