a38cc528c0ade2abce764925dd0d7e001c97ca53
SiamAnzir <siamanzir18@gmail.com>
2020-06-09 11:44:52 +0600

    Modal_added

--- js/index.js
+++ js/index.js
@@ -1,49 +1,97 @@
 $(document).ready(function() {
-    function getUserPostsUi(posts) {
-        $(".pageloader").fadeOut("slow");
 
-        var str = "";
-        var i;
-        for (i = 2; i < 5; i++) {
-
-
-            str = str + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
-                '<div class="card" style="height:320px;width:250px">' +
-                '<h4 class="card-header"><b>' + posts[i].title.substr(0, 30) + '</b></h4>' +
-                '<div class="card-body">' +
-                '<h5 class="card-title"><b> Description</b></h5>' +
-                '<p class="card-text">' + posts[i].body.substr(0, 80) + '.........' + '</p>' +
-                '</div>' +
-                '<div class="card-footer">' +
-                '<button type="button" class="btn btn-primary" data-id="" data-toggle="modal" data-target="#exampleModal">Read More</button>' +
-                '</div>' +
-                '</div>' +
-                '</div>';
-        }
-        $("#cardId").html(str);
-    }
-    var $post;
-    //var i = 0;
     $.ajax({
         method: 'GET',
         url: 'https://jsonplaceholder.typicode.com/posts'
     }).done(function(post) {
-        console.log(post);
 
         getUserPostsUi(post);
     }).fail(function(error) {
         console.log(error);
     });
 
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/todos'
+    }).done(function(todo) {
+        console.log(todo);
+
+        getUserTodosUi(todo);
+    }).fail(function(error) {
+        console.log(error);
+    });
+
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/photos'
+    }).done(function(photo) {
+        console.log(photo[0]);
+
+        getUserPhotosUi(photo);
+    }).fail(function(error) {
+        console.log(error);
+    });
+
+
+    function getUserPostsUi(posts) {
+        $(".pageloader").fadeOut("slow");
+
+        let singlePostCard = "";
+        for (let i = 0; i < 5; i++) {
+            singlePostCard = singlePostCard +
+                '<div class="col-lg-4 col-md-6 col-sm-12" >' +
+                '<div class="card border-dark mb-3" style="height:320px;width:250px">' +
+                '<h4 class="card-header"><b>' + posts[i].title.substr(0, 30) + '</b></h4>' +
+                '<div class="card-body">' +
+                '<h5 class="card-title"><b> Description</b></h5>' +
+                '<p class="card-text">' + posts[i].body.substr(0, 80) + '.........' + '</p>' +
+                '</div>' +
+                '<div class="card-footer">' +
+                '<button type="button"+ data-id="' + i + '" class="btn btn-primary" id="btn"  data-toggle="modal" data-target="#myModal">Read More</button>' +
+                '<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
+                '<div class="modal-dialog" role="document">' +
+                '<div class="modal-content">' +
+                '<div class="modal-header">' +
+                '<h5 class="modal-title" id="exampleModalLabel"></h5>' +
+                '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+                '</div>' +
+                '<div class="modal-body">' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+        }
+
+        $("#postContainer").html(singlePostCard);
+
+
+        $(".card-footer").on("click", "button", function() {
+            var bid = $(this).attr("data-id");
+            $('.modal-title').html(posts[bid].title);
+            $('.modal-body').html("<b> Description: </b>" + posts[bid].body);
+            $('#myModal').modal('show');
+        });
+    }
+
+
+
+
     function getUserPhotosUi(photos) {
         $(".pageloader").fadeOut("slow");
-        var pht = "";
-        var i;
-        for (i = 0; i < 6; i++) {
+        let singlePhoto = "";
 
-            pht = pht + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
+        for (let i = 0; i < 6; i++) {
+            singlePhoto = singlePhoto + '<div class="col-lg-4 col-md-6 col-sm-12" >' +
                 '<div class="card-group">' +
-                '<div class="card border-dark mb-3" style="max-width: 18rem;">' +
+                '<div class="card border-dark mb-3" style="width: 18rem;">' +
                 '<img class="card-img-top" src="' + photos[i].thumbnailUrl + '" alt="Card image cap">' +
                 '<div class="card-body text-dark">' +
                 '<h5 class="card-title"><b> Photo title </b></h5>' +
@@ -58,29 +106,18 @@ $(document).ready(function() {
 
         }
 
-
-        $("#photoId").html(pht);
+        $("#photoContainer").html(singlePhoto);
     }
-    var $photo;
-    //var i = 0;
-    $.ajax({
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/photos'
-    }).done(function(photo) {
-        console.log(photo[0]);
 
-        getUserPhotosUi(photo);
-    }).fail(function(error) {
-        console.log(error);
-    });
+
 
     function getUserTodosUi(todos) {
         $(".pageloader").fadeOut("slow");
-        var tds = "";
-        var i;
-        for (i = 0; i < 10; i++) {
+        let singleTodo = "";
 
-            tds = tds + '<tr>' +
+        for (let i = 0; i < 10; i++) {
+
+            singleTodo = singleTodo + '<tr>' +
                 '<th scope="row">' + todos[i].id + '</th>' +
                 '<td>' + todos[i].title + '</td>' +
                 '<td>' +
@@ -93,18 +130,7 @@ $(document).ready(function() {
         }
 
 
-        $("#todoId").html(tds);
+        $("#todoContainer").html(singleTodo);
     }
-    var $post;
-    //var i = 0;
-    $.ajax({
-        method: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/todos'
-    }).done(function(todo) {
-        console.log(todo);
 
-        getUserTodosUi(todo);
-    }).fail(function(error) {
-        console.log(error);
-    });
 });
